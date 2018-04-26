a660d1f0baa1295c4c7d87ba2cbc1860508eb5f8
Fábio Chicout <fchicout@gmail.com>
2018-04-26 20:56:33 -0300

    First commit. Mocks of the first desired funcions

---
+++ LDAP_PoSH.ps1
@@ -0,0 +1,39 @@
+﻿$plainLdapConnection = $null;
+
+function New-LdapSession{
+param(
+    $Server, 
+    $Login,
+    $Password,
+    $TlsEnabled
+    )
+
+}
+
+function Get-LdapSchemas{
+param()
+
+}
+
+function List-LdapSchemas{
+param()
+
+}
+
+function Set-LdapSchemas{
+param()
+
+}
+
+function Remove-LdapSchemas{
+param()
+
+}
+
+function List-LdapObject{
+param()
+
+}
+
+
+#Export-ModuleMember -Function 'New-LdapSession'
